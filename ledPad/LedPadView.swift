44635d85db45a535d416b99a0a002574617bc847
lisper <leyapin@gmail.com>
2016-01-15 18:56:34 +0800

    draw [[]], add new feature

--- ledPad/LedPadView.swift
+++ ledPad/LedPadView.swift
@@ -12,10 +12,12 @@ class LedPadView: UIView {
 
     //当前触摸点
     var fingerPoint = CGPoint()
-    //已划过的点集合
+    //所有点对象映射
     var points:[CGPoint] = [CGPoint]()
+    //
+    var drawPoints:[[CGPoint]] = [[CGPoint]]()
     //已划过的点的下标
-    var selectIndexs:[Int] = [Int]()
+    var selectIndexs:[[Int]] = [[Int]]()
     //圆半径
     var circleRadius:CGFloat!
     //圆中心距
@@ -27,9 +29,10 @@ class LedPadView: UIView {
     //led的边长(一排的数量)
     var ledLength:Int = 12
     //靠边的距离
-    var sideWidth:CGFloat = 20
+    var sideWidth:CGFloat = 10
     //两个圆边与边的距离
-    var circleSideDistance:CGFloat = 5
+    var circleSideDistance:CGFloat = 10
+    var sideLength:CGFloat!
     
     
     override init(frame: CGRect) {
@@ -39,12 +42,26 @@ class LedPadView: UIView {
         self.backgroundColor = UIColor(red: 70/255, green: 80/255, blue: 60/255, alpha: 1)
         circleRadius = (screenW-self.circleSideDistance*CGFloat(self.ledLength-1)-self.sideWidth*2.0) / CGFloat(self.ledLength*2)
         centerDistance = (screenW-self.sideWidth*2.0 - circleRadius*2)/CGFloat(self.ledLength-1)
+        sideLength = screenW-self.sideWidth*2
         firstPointX = sideWidth + circleRadius
         firstPointY = 140
         print("R = \(circleRadius)")
         fillPoints()
+        
+        let clearButton = UIButton(type: UIButtonType.Custom)
+        clearButton.frame = CGRect(x: sideWidth, y: self.firstPointY+sideLength, width: 100, height: 40)
+        clearButton.backgroundColor = UIColor(red: 80/255, green: 90/255, blue: 40/255, alpha: 1)
+        clearButton.setTitle("clear", forState: UIControlState.Normal)
+        clearButton.titleLabel?.font = UIFont.systemFontOfSize(28)
+        clearButton.addTarget(self, action: "clearActive", forControlEvents: UIControlEvents.TouchUpInside)
+        self.addSubview(clearButton)
     }
     
+    func clearActive() {
+        self.selectIndexs.removeAll(keepCapacity: true)
+        self.drawPoints.removeAll(keepCapacity: true)
+        self.setNeedsDisplay()
+    }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -52,17 +69,23 @@ class LedPadView: UIView {
     
     override func drawRect(rect: CGRect) {
         for i in 0..<self.selectIndexs.count {
-            if (i+1) < self.selectIndexs.count {
-            let l1 = self.selectIndexs[i]
-            let l2 = self.selectIndexs[i+1]
-                print("\(l1), \(l2)")
-            drawLine(points[l1], p2: points[l2])
+            for j in 0..<self.selectIndexs[i].count {
+                if (j+1) < self.selectIndexs[i].count {
+                    let l1 = self.selectIndexs[i][j]
+                    let l2 = self.selectIndexs[i][j+1]
+                    drawLine(points[l1], p2: points[l2])
+                }
             }
         }
-        if (self.selectIndexs.count > 0) {
-            drawLine(points[self.selectIndexs.last!], p2: self.fingerPoint)
-        }
+        
+        let lastIndex = self.selectIndexs.count-1
+            if ( lastIndex >= 0 && self.selectIndexs[lastIndex].count > 0) {
+                drawLine(points[self.selectIndexs[lastIndex].last!], p2: self.fingerPoint)
+            }
         drawCircles()
+        for i in 0..<self.drawPoints.count {
+            drawLines(self.drawPoints[i], color: UIColor.greenColor())
+        }
     }
     
     func fillPoints () {
@@ -85,7 +108,7 @@ class LedPadView: UIView {
         bp.stroke()
     }
     
-    func drawLines(lines:[CGPoint]) {
+    func drawLines(lines:[CGPoint], color:UIColor) {
         if (lines.count < 2) {
             return
         }
@@ -93,8 +116,8 @@ class LedPadView: UIView {
         bp.lineWidth = 4
         bp.lineCapStyle = .Round
         //UIColor(red: 0.4, green: 0.5, blue: 0.4, alpha: 1).setStroke()
-        UIColor.redColor().setStroke()
-        UIColor.redColor().setFill()
+        color.setStroke()
+        color.setFill()
         
         bp.moveToPoint(lines[0])
         for i in 0..<lines.count {
@@ -117,9 +140,9 @@ class LedPadView: UIView {
         CGContextAddArc(context, point.x, point.y, self.circleRadius, 0, CGFloat(M_PI*2), 1)
         //CGContextStrokePath(context)
         CGContextFillPath(context)
-        CGContextSetLineWidth(context, 4.0)
-        CGContextAddArc(context, point.x, point.y, self.circleRadius-2.0, 0, CGFloat(M_PI*2), 1)
-        CGContextStrokePath(context)
+        // CGContextSetLineWidth(context, 4.0)
+        // CGContextAddArc(context, point.x, point.y, self.circleRadius-2.0, 0, CGFloat(M_PI*2), 1)
+        // CGContextStrokePath(context)
     }
     
     func drawCircles() {
@@ -133,7 +156,10 @@ class LedPadView: UIView {
     }
     
     func isInside (p:CGPoint, _ cp:CGPoint) -> Bool {
-        if p.x>(cp.x-self.circleRadius) && p.x<(cp.x+self.circleRadius) && p.y>(cp.y-self.circleRadius) && p.y<(cp.y+self.circleRadius) {
+        if p.x>(cp.x-self.circleRadius)
+            && p.x<(cp.x+self.circleRadius)
+            && p.y>(cp.y-self.circleRadius)
+            && p.y<(cp.y+self.circleRadius) {
            return true
         }
         return false
@@ -141,45 +167,59 @@ class LedPadView: UIView {
     
     func contains(index:Int) -> Bool {
         for i in 0..<self.selectIndexs.count {
-            if index == selectIndexs[i] {
-                return true
+            for j in 0..<self.selectIndexs[i].count {
+                if index == self.selectIndexs[i][j] {
+                    return true
+                }
             }
         }
         return false
     }
     
-    func processPoint(point:CGPoint) {
-        for i in 0..<self.points.count {
+        func processPoint(point:CGPoint) {
+            var lastIndex = self.selectIndexs.count-1
+            if lastIndex < 0 {
+                lastIndex = 0
+                self.selectIndexs.append([Int]())
+            }
+            for i in 0..<self.points.count {
             if (!contains(i)) {
                 //if distance(point, points[i]) <= circleRadius {
                 if isInside(point, points[i]) {
-                    self.selectIndexs.append(i)
+                    self.selectIndexs[lastIndex].append(i)
                 }
             }
         }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.selectIndexs.removeAll(keepCapacity: false)
+        //self.selectIndexs.removeAll(keepCapacity: false)
+        self.selectIndexs.append([Int]())
         self.fingerPoint = (touches.first?.locationInView(self))!
+        self.drawPoints.append([CGPoint]())
+        let lastIndex = self.drawPoints.count-1
+        self.drawPoints[lastIndex].append(fingerPoint)
         processPoint(fingerPoint)
         self.setNeedsDisplay()
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.fingerPoint = (touches.first?.locationInView(self))!
-        let prePoint = (touches.first?.previousLocationInView(self))!
-        let midPoint = CGPoint(x: (self.fingerPoint.x-prePoint.x)/2.0, y: (self.fingerPoint.y-prePoint.y)/2.0)
-        processPoint(midPoint)
+        let lastIndex = self.drawPoints.count-1
+        self.drawPoints[lastIndex].append(fingerPoint)
         processPoint(fingerPoint)
         self.setNeedsDisplay()
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if self.selectIndexs.count > 0 {
-            self.fingerPoint = self.points[self.selectIndexs.last!]
-            self.setNeedsDisplay()
+        let touchPoint = (touches.first?.locationInView(self))!
+        let lastIndex = self.drawPoints.count-1
+        self.drawPoints[lastIndex].append(touchPoint)
+        let lastIndex1 = self.selectIndexs.count-1
+        if let pointLast = self.selectIndexs[lastIndex1].last {
+            self.fingerPoint = self.points[pointLast]
         }
+        self.setNeedsDisplay()
     }
     
     override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
